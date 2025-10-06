b0eccdd64996c13ad39a6044aa12099b66021eb2
mek <mek>
2025-10-06 14:45:25 +0700

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Button from "$lib/components/ui/button/button.svelte";
+</script>
+
+<h1>Welcome to Metrindo Landing Page</h1>
+<p>
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+	documentation
+</p>
+
+<Button>tes</Button>
