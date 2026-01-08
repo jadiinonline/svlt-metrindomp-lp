1ed2e7f4d03df9d0e9b5d10043b27dade755b4e1
mek <mek>
2026-01-08 11:15:59 +0700

    update dependencies and working wuchale

---
+++ src/lib/localstorage.ts
@@ -0,0 +1,18 @@
+import { persisted } from 'svelte-persisted-store';
+// import { writable } from 'svelte/store';
+// export const masterPassword = writable<Uint8Array | null>(null);
+// export const computationIntensity = persisted<number>('computationIntensity', 3);
+// export interface Site {
+// 	email: string;
+// 	domain: string;
+// 	rotationRounds: number;
+// 	comment: string;
+// }
+// export const sites = persisted<Site[]>('passwordManagerSites', []);
+
+// export const lastAuthType = persisted<'password' | 'bip39'>('lastAuthType', 'password');
+export const language = persisted<string>('language', 'id');
+
+if (typeof window !== 'undefined' && !localStorage.getItem('language')) {
+	language.set('id');
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { wuchale } from '@wuchale/vite-plugin'
 
 
 export default defineConfig({
 	plugins: [
+		wuchale(),
 		tailwindcss(),
-		sveltekit()
+		sveltekit(),
 	]
 });
